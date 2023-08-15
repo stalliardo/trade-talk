27765dcf58519bfee526b0ba4a00be041619b8eb
stalliardo187 <darrenstallard27@hotmail.com>
2023-08-15 18:32:33 +0100

    Added the google sign in provider and used from the navbar signIn, signOut functions

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Auth from "@components/Auth";
 export default function Home() {
   return (
    <main>
-      <Auth />
+     
    </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,10 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut, signIn } from 'next-auth/react';
 
 const Navbar = () => {
-    const {data: session} = useSession();
+    const { data: session } = useSession();
     return (
         <nav className='bg-slate-700 p-4 flex justify-between'>
             {/* icon */}
@@ -16,15 +16,15 @@ const Navbar = () => {
             {/* links */}
             <div className='flex items-center'>
                 {
-                    session === null && <p className='mr-4 pr-4 border-r border-orange-600 hover:text-orange-500 cursor-pointer'>Stalliardo</p>
+                    session !== null && <p className='mr-4 pr-4 border-r border-orange-600 hover:text-orange-500 cursor-pointer'>{session?.user.name}</p>
                 }
                 <Link href="/" className='mr-3 hover:text-orange-500'>About</Link>
                 <Link href="/" className='mr-3 hover:text-orange-500'>Contact</Link>
 
                 {
                     session !== null ?
-                    <Link href="/" className='mr-3 hover:text-orange-500'>Log Out</Link> :
-                    <Link href="/" className='mr-3 hover:text-orange-500'>Log In</Link>
+                        <p className='mr-4 pr-4 hover:text-orange-500 cursor-pointer' onClick={() => signOut()}>Log Out</p> :
+                        <p className='mr-4 pr-4 hover:text-orange-500 cursor-pointer' onClick={() => signIn()}>Log In</p>
                 }
             </div>
         </nav>
