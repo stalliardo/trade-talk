465ce63c480bee29f0a490a8db4692c57b16f52c
stalliardo187 <darrenstallard27@hotmail.com>
2023-08-17 15:30:11 +0100

    Posted the anser to the /api/question/answer route

---
+++ app/api/question/answer/route.ts
@@ -0,0 +1,33 @@
+import { connectToDB } from "@utils/database";
+import Question from "@models/question";
+import Answer from "@models/Answer"; // Import your Answer model
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    if (req.method === "POST") {
+        try {
+            await connectToDB();
+            // const { id } = req.query;
+            const { text, questionId } = await req.json();
+            
+            // Create a new Answer document
+            const newAnswer = new Answer({ text, question: questionId });
+            await newAnswer.save();
+
+            // Update the answers field in the corresponding question
+            await Question.findByIdAndUpdate(
+                questionId,
+                { $push: { answers: newAnswer._id } } // Add the answer's ID to the answers array
+            );
+
+            return NextResponse.json({message: "Answer successfully created!"}, { status: 201});
+
+        } catch (error) {
+            return NextResponse.json({error: "Error posting answer"}, { status: 500});
+            
+        }
+    } else {
+        return NextResponse.json({error: "Method not allowed"}, { status: 405});
+    }
+}
--- app/question/view/[id]/page.tsx
+++ app/question/view/[id]/page.tsx
@@ -1,20 +1,73 @@
 "use client";
 
-import { useEffect } from 'react'
+import { QuestionData } from '@types';
+import { useEffect, useState } from 'react'
 
-const ViewQuestionPage = ({params}: { params: {id: string}}) => {
+const ViewQuestionPage = ({ params }: { params: { id: string } }) => {
 
-    // useEffect(() => { // TODO reinstate
-    //     const incrementViews = async () => {
-    //         const response = await fetch(`/api/question/view/${params.id}`);
-    //         const data = await response.json();
-    //     }
+  const [question, setQuestion] = useState<QuestionData | null>(null);
 
-    //     incrementViews();
-    // }, [])
+  useEffect(() => {
+      const getQuestion = async () => {
+          const response = await fetch(`/api/question/view/${params.id}`);
+          const data = await response.json();
+
+          console.log("data from /apiquestion/view = ", data.question);
+
+          setQuestion(data.question);
+      }
+
+      getQuestion();
+  }, [])
+
+  const [answer, setAnswer] = useState("");
+
+  const handleAnswerSubmitted = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("submit anser called");
+
+    try {
+        const response = await fetch(`/api/question/answer`, {
+          method: "POST",
+          body: JSON.stringify({
+            text: answer,
+            questionId: params.id
+          })
+        });
+
+        const data = await response.json();
+
+        console.log("data from api/question/answer =  ", data);
+    } catch (error) {
+        console.log("error caught = ", error);
+    }
+  }
+
   return (
-    <div>ViewQuestionPage</div>
+    <section>
+      <div className="max-w-7xl border mt-16 pl-32">
+        <h1 className='text-2xl'>{question?.title}</h1>
+        <p className='text-gray-300 text-lg mb-16'>
+          {question?.question}
+        </p>
+
+        {/* if authed */}
+        <p>Answer this question:</p>
+        <form className='flex flex-col' onSubmit={handleAnswerSubmitted}>
+          <textarea
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            className='input_bg w-1/2'
+            placeholder='min 16 characters'
+          />
+
+          <button type='submit' className='button_bg w-1/4 mt-4' disabled={answer.length < 16}>Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default ViewQuestionPage
+export default ViewQuestionPage;
+
