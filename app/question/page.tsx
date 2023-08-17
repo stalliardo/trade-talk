692699a32dc484943171f3feb962d24f298118fa
stalliardo187 <darrenstallard27@hotmail.com>
2023-08-17 10:36:52 +0100

    added the search api and called from the forms onsubmit function

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,11 +1,24 @@
+import Search from '@components/Search'
+import SearchResults from '@components/SearchResults'
 import React from 'react'
 
 const QuestionHomePage = () => {
   return (
     <section>
-        <div className="h-56 text-center">
+      <div className="h-[150px] text-center">
         <h1 className="text-6xl mt-16"><span className="orange_gradient font-bold">TradeTalk</span> Questions</h1>
-        <p className="text-lg mt-2">Have a trade related question? Well, you've come to the right place. Search or browse the questions below.</p>
+        <p className="text-lg mt-6">Have a trade related question? Well, you've come to the right place. Search or browse the questions below.</p>
+      </div>
+
+      <div className="h-72 border text-center px-20 max-w-7xl mx-auto mt-10">
+        <div>
+          <h2 className='text-xl text-left'>Ask Tradesman for Advice:</h2>
+          <Search />
+        </div>
+
+        <div>
+          <SearchResults />
+        </div>
       </div>
     </section>
   )
