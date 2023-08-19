5aad933bc1fb2f8f5db8212f8aa5f84d7ac87816
stalliardo187 <darrenstallard27@hotmail.com>
2023-08-19 08:16:27 +0100

    Added a generic table component to display question and answer data

---
+++ components/table/GenericTable.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+// Define the props for the table
+interface TableProps<T> {
+  data: T[];
+  headers: string[];
+  renderRow: (item: T, index: number) => React.ReactNode;
+  onEdit?: (item: T) => void;
+  onDelete?: (item: T) => void;
+  onView?: (item: T) => void;
+}
+
+// Create the generic table component
+function GenericTable<T>({ data, headers, renderRow, onEdit, onDelete, onView }: TableProps<T>) {
+  return (
+    <table className="min-w-full divide-y divide-gray-800 border">
+    <thead className="bg-custom_secondary border-b border-custom_border">
+      <tr>
+        {headers.map((header, index) => (
+          <th
+            key={index}
+            scope="col"
+            className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+            {header}
+          </th>
+        ))}
+        {onEdit && (
+          <th scope="col" className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Edit
+          </th>
+        )}
+        {onDelete && (
+          <th scope="col" className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Delete
+          </th>
+        )}
+        {onView && (
+          <th scope="col" className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            View
+          </th>
+        )}
+      </tr>
+    </thead>
+    <tbody className="bg-custom_secondary">
+      {data.map((item, index) => (
+        <tr key={index} className='border-b border-custom_border'>
+          {renderRow(item, index)}
+          {onEdit && (
+            <td className="py-4 whitespace-nowrap text-sm font-medium">
+              <button
+                onClick={() => onEdit(item)}
+                className="text-indigo-600 hover:text-indigo-900"
+              >
+                Edit
+              </button>
+            </td>
+          )}
+          {onDelete && (
+            <td className="py-4 whitespace-nowrap text-sm font-medium">
+              <button
+                onClick={() => onDelete(item)}
+                className="text-red-600 hover:text-red-900"
+              >
+                Delete
+              </button>
+            </td>
+          )}
+          {onView && (
+            <td className="py-4 whitespace-nowrap text-sm font-medium">
+              <button
+                onClick={() => onView(item)}
+                className="text-blue-600 hover:text-blue-900"
+              >
+                View
+              </button>
+            </td>
+          )}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  );
+}
+
+export default GenericTable;
--- components/user/UsersQuestions.tsx
+++ components/user/UsersQuestions.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import DisplayQuestions from '@components/DisplayQuestions';
+import GenericTable from '@components/table/GenericTable';
 import { QuestionData } from '@types';
 import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react'
@@ -10,19 +11,32 @@ interface UserQuestionProps {
   data: QuestionData[];
 }
 
-const UsersQuestions = ({data}: UserQuestionProps) => {
+const UsersQuestions = ({ data }: UserQuestionProps) => {
 
   console.log("data from questions = ", data);
 
+  const handleEdit = (question: QuestionData) => {
+
+  }
+  const handleDelete = (question: QuestionData) => {
+
+  }
   return (
-    <div className='border w-full'>
-        {
-          data.length ? 
-          <div>
-            <DisplayQuestions data={data}/>
-          </div> : 
-          <div></div>
-        }
+    <div className='w-full mt-4'>
+      <GenericTable<QuestionData>
+        data={data}
+        headers={['Title', 'Category', 'Views', 'Answers']}
+        renderRow={(question) => (
+          <>
+            <td className=''>{question.title}</td>
+            <td className=''>{question.category}</td>
+            <td className=' w-[10%]'>{question.views.length}</td>
+            <td className=' w-[10%]'>{question.answers.length}</td>
+          </>
+        )}
+        onEdit={(question) => handleEdit(question)}
+        onDelete={(question) => handleDelete(question)}
+      />
     </div>
   )
 }
